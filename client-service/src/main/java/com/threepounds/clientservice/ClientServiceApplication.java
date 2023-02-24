84fa2f337684fc50b1fb2a3863819da29ad48a98
bkantarRDC <bkantar@idbs.com>
2023-02-24 15:48:17 +0300

    client services created

--- client-service/src/main/java/com/threepounds/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/threepounds/clientservice/ClientServiceApplication.java
@@ -3,7 +3,7 @@ package com.threepounds.clientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.threepounds")
 public class ClientServiceApplication {
 
     public static void main(String[] args) {
